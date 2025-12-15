e07f84889563d6eb121a14be7f5f366f94dbbca5
nfworking <tidalfarmer1432@outlook.com>
2025-12-15 11:43:35 +1000

    ghfg

--- automation/dns/check-dns.tf
+++ automation/dns/check-dns.tf
@@ -1,6 +1,6 @@
 resource "dns_a_record_set" "admin" {
     zone      = "lurking.site."
-    name      = "admin.home"
+    name      = "admin"
     addresses = ["172.20.0.254"]
     ttl       = 300
 }
