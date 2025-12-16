b103797cb3f48ab84dbf5bb4ab022fe8572811a8
nfworking <tidalfarmer1432@outlook.com>
2025-12-16 14:02:20 +1000

    test another

--- automation/dns/check-dns.tf
+++ automation/dns/check-dns.tf
@@ -4,3 +4,10 @@ resource "dns_a_record_set" "admin" {
     addresses = ["172.20.0.254"]
     ttl       = 300
 }
+
+resource "dns_a_record_set" "adminhome" {
+    zone      = "lurking.site."
+    name      = "admin.home"
+    addresses = ["172.20.0.254"]
+    ttl       = 300
+}
