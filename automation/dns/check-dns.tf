4ea92bc431b271cf4efab809a8d80a306cede43a
Cal <168704009+nfworking@users.noreply.github.com>
2025-12-20 10:09:30 +1000

    added more records

--- automation/dns/check-dns.tf
+++ automation/dns/check-dns.tf
@@ -26,3 +26,23 @@ resource "dns_a_record_set" "automation" {
     ttl       = 300
 }
 
+resource "dns_a_record_set" "mail" {
+    zone      = "lurking.site."
+    name      = "mail.home"
+    addresses = ["172.20.0.254"]
+    ttl       = 300
+}
+
+resource "dns_a_record_set" "pro" {
+    zone      = "lurking.site."
+    name      = "pro.home"
+    addresses = ["172.20.0.254"]
+    ttl       = 300
+}
+
+resource "dns_a_record_set" "pro_api" {
+    zone      = "lurking.site."
+    name      = "api.pro.home"
+    addresses = ["172.20.0.254"]
+    ttl       = 300
+}
